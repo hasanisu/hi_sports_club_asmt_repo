8c435bcc0a0023562acb699ac7f91292aba47097
hasanisu <hasanjimam@gmail.com>
2023-01-19 16:38:50 +0900

    updated cart

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -36,12 +36,16 @@ const Home = () => {
 
     return (
         <div>
-
+            
             <div className='grid lg:grid-cols-4 sm:grid-cols-1'>
-
+                
                 
                 <div className='bg-slate-100 col-span-3'>
-                    <h2 className='mt-48 text-left ml-12 mb-12 text-2xl font-bold text-red-400'>Badminton Sports Club</h2>
+                    <div className='flex mt-36 ml-10'>
+                        <img className='w-14 border rounded-3xl' src="https://t3.ftcdn.net/jpg/00/54/89/08/360_F_54890802_j6XIBBYEpMf9D5mTZsSrqmXmJCtT3X0d.jpg" alt="" />
+                        <h2 className='text-left ml-3 mt-2 text-orange-600 text-3xl font-semibold'>HI-SPORTS-CLUB</h2>
+                    </div>
+                    <h2 className='mt-10 text-left ml-10 mb-12 text-2xl'>Badminton Sports Club</h2>
                     
                     <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-2'>
                     {
